d055e831a9ecb8ccca0a330e57e892a4af9abd7a
nkluba <lubandust@gmail.com>
2025-02-22 12:31:25 +0200

    Add project files.

---
+++ WebApplication2/Models/Activity.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsImportant { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
